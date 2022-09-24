5a06f4cc3fb1c744848a253d427383df36192aba
Alexandru Florea <alexflorea16000@gmail.com>
2022-09-24 15:17:45 +0300

    http request and headers reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead}};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:2020")?;
+    println!("Listening on port 2020...");
+
+    for stream in listener.incoming() {
+        handle_connection(stream?);
+    }
+
+    Ok(())
+}
+
+fn handle_connection(stream: TcpStream) {
+    let mut reader = BufReader::new(&stream);
+
+    let mut request_line = String::new();
+    reader.read_line(&mut request_line).unwrap();
+    request_line = request_line.trim().to_string();
+
+    let header_lines: Vec<_> = reader.lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    // TODO: parse headers and read body
+    // let mut body_bytes = Vec::with_capacity(capacity)
+
+    println!("Req: {request_line}");
+    println!("Headers: {header_lines:?}");
 }
